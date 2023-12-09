a47c26acd67428677da02a8692ebd956895d2b6a
dzolovs <153331025+dzolovs@users.noreply.github.com>
2023-12-09 09:54:15 +0100

    .

---
+++ Controllers/BookController.cs
@@ -0,0 +1,176 @@
+﻿using Booker.Models;
+using Booker.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+
+
+namespace Booker.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBooksRepository bookRepository;
+
+        public BookController(IBooksRepository booksRepository)
+        {
+            bookRepository = booksRepository;
+        }
+
+        // GET: BookController
+        [Authorize]
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.TitleSortParm = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.AuthorSortParm = String.IsNullOrEmpty(sortOrder) ? "author_desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var books = from b in bookRepository.GetBooks()
+                        select b;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(b => b.Title.ToUpper().Contains(searchString.ToUpper())
+                                       || b.Author.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    books = books.OrderByDescending(b => b.Title);
+                    break;
+                case "author_desc":
+                    books = books.OrderByDescending(b => b.Author);
+                    break;
+                default:
+                    books = books.OrderBy(b => b.Title);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(books);
+        }
+
+
+        // GET: BookController/Details/5
+        [AllowAnonymous]
+        public ActionResult Details(int id)
+        {
+            return View(bookRepository.GetBookByID(id));
+        }
+
+
+        // GET: BookController/Create
+        [Authorize(Roles = "admin")]
+        public ActionResult Create()
+        {
+            return View(new Books());
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Books bookModel)
+        {
+            bookRepository.InsertBook(bookModel);
+            return RedirectToAction(nameof(Index));
+        }
+        // [Authorize(Roles = "admin")]
+        // GET: BookController/Edit/5
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            return View(bookRepository.GetBookByID(id));
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Books bookModel)
+        {
+            bookRepository.UpdateBook(id, bookModel);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        // GET: BookController/Delete/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int id)
+        {
+            return View(bookRepository.GetBookByID(id));
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Books bookModel)
+        {
+            bookRepository.DeleteBook(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: BookController/Sell/5
+        [Authorize(Roles = "manager, admin")]
+        public ActionResult Sell(int id)
+        {
+            return View(bookRepository.GetBookByID(id));
+        }
+
+        //POST: BookController/Buy/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Sell(int id, Books bookModel)
+        {
+            if (bookRepository.SellBook(id, bookModel))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["Message"] = "This book is not in stock";
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: BookController/Catalog
+        [AllowAnonymous]
+        public ActionResult Catalog(string sortOrder, string currentFilter, string searchString)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.TitleSortParm = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.AuthorSortParm = String.IsNullOrEmpty(sortOrder) ? "author_desc" : "";
+
+            ViewBag.CurrentFilter = searchString;
+
+            var books = from b in bookRepository.GetBooksInStock()
+                        select b;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(b => b.Title.ToUpper().Contains(searchString.ToUpper())
+                                       || b.Author.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    books = books.OrderByDescending(b => b.Title);
+                    break;
+                case "author_desc":
+                    books = books.OrderByDescending(b => b.Author);
+                    break;
+                default:
+                    books = books.OrderBy(b => b.Title);
+                    break;
+            }
+            return View(books);
+        }
+
+
+    }
+}
---
+++ Repositories/IBooksRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Booker.Models;
+
+namespace Booker.Repositories
+{
+    public interface IBooksRepository
+    {
+       // Books Get(int bookId);
+        IEnumerable<Books> GetBooks();
+        Books GetBookByID(int bookId);
+        void InsertBook(Books book);
+        void DeleteBook(int bookID);
+        void UpdateBook(int bookId, Books book);
+        bool SellBook(int bookId, Books book);
+        IQueryable<Books> GetBooksInStock();
+
+    }
+}
